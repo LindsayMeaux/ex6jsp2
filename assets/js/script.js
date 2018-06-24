ff902fc452b193773cf409566d8fa7a0ab10b65c
lindsaym <lindsay.meaux@gmail.com>
2018-06-24 08:04:33 +0200

    modif js + ajoue img

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function imgChange(img){
+  document.getElementById('image1').src = img;
+}
+function img2Change(img)
+{document.getElementById('image2').src = img;
+}
+function img3Change(img) {
+document.getElementById('image3').src = img;
+}
+function img4Change(img) {
+document.getElementById('image4').src = img;
+}
+function img5Change(img) {
+document.getElementById('image5').src = img;
+}
